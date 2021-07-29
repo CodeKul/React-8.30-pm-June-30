a64616707005b8a64cd9a289d69a058dc007dfb1
sanketcodekul <sanketbhosale.codekul.com>
2021-07-29 23:14:39 +0530

    redux

--- redux-ex/src/App.js
+++ redux-ex/src/App.js
@@ -8,7 +8,9 @@ import AccountType from './components/AccountType';
 // https://blog.logrocket.com/using-recompose-to-write-clean-higher-order-components-3019a6daf44c/
 // https://medium.com/@mrewusi/reusing-logic-in-your-react-apps-with-higher-order-components-70cdb8e7230a
 // https://medium.com/@mjewell/maximizing-higher-order-component-reusability-289950410b83
-//
+// https://material-ui.com/getting-started/installation/
+// https://ant.design/
+// https://blog.logrocket.com/the-best-styling-in-react-tutorial-youve-ever-seen-676f1284b945/
 
 function App() {
   return (
